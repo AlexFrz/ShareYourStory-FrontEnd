56b359169b408f623ad971f2edafcf523bb006d0
Alex <frezoul.alex@gmail.com>
2020-11-04 17:55:19 +0100

    initialization and react-router-dom

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home}></Route>
+          <Route exact path="/login" component={login}></Route>
+          <Route exact path="/signup" component={signup}></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
