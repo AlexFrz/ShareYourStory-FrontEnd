9bb66afda6cdecea9e1654d090e4036f3d338117
Alex <frezoul.alex@gmail.com>
2020-11-07 17:30:21 +0100

    front end: home & login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+// Components
+import Navbar from "./components/Navbar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: "#FE8171", contrastText: "#FBF5EF" },
+    secondary: { main: "#109386", contrastText: "#fff" },
+    tercary: "#61745A",
+    dark: "#2C2A29",
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={home}></Route>
-          <Route exact path="/login" component={login}></Route>
-          <Route exact path="/signup" component={signup}></Route>
-        </Switch>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="app">
+        <Router>
+          <Navbar />
+          <div className="app__container">
+            <Switch>
+              <Route exact path="/" component={home}></Route>
+              <Route exact path="/login" component={login}></Route>
+              <Route exact path="/signup" component={signup}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,42 @@
+import Grid from "@material-ui/core/Grid";
 import React, { Component } from "react";
+import axios from "axios";
+import Story from "../components/Story";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 class home extends Component {
+  state = {
+    stories: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/stories")
+      .then((res) => {
+        this.setState({ stories: res.data });
+      })
+      .catch((err) => console.log(err));
+  }
   render() {
+    let recentStoryMarkup = this.state.stories ? (
+      this.state.stories.map((story) => (
+        <Story key={story.storyId} story={story} />
+      ))
+    ) : (
+      <p> Loading...</p>
+    );
+
     return (
       <div>
-        <h1>home page</h1>
+        <Grid container spacing={16}>
+          <Grid item sm={8} xs={12}>
+            {recentStoryMarkup}
+          </Grid>
+          <Grid item sm={4} xs={12}>
+            <p>Profile...</p>
+          </Grid>
+        </Grid>
       </div>
     );
   }
